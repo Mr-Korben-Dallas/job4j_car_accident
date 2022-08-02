3a620e17d20da01d1d0dd5a1fbb9abcfb81d2740
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-08-02 08:24:20 +0400

    [#295782]:
    - added table view for index page;

--- src/main/java/ru/job4j/accident/controller/IndexController.java
+++ src/main/java/ru/job4j/accident/controller/IndexController.java
@@ -1,12 +1,24 @@
 package ru.job4j.accident.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        List<String> strings = Arrays.asList(
+                "lubas cadunt!",
+                "guttuss persuadere!",
+                "accentors experimentum!",
+                "lubas volare!",
+                "extums ortum!"
+        );
+        model.addAttribute("strings", strings);
         return "index";
     }
 }
