c82c014580d6a3e7561f0bc07491a0bc115542ae
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-08-03 10:22:24 +0400

    [#305522 #328159]:
    - refactoring;

--- src/main/java/ru/job4j/accident/repository/AccidentTypeMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentTypeMem.java
@@ -2,23 +2,21 @@ package ru.job4j.accident.repository;
 
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.AccidentType;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class AccidentTypeMem {
     private final AtomicLong accidentTypeId = new AtomicLong();
-    List<AccidentType> accidentTypes = new ArrayList<>();
+    Map<Long, AccidentType> accidentTypes = new HashMap<>();
     
     public AccidentTypeMem() {
-        accidentTypes.add(AccidentType.of(accidentTypeId.incrementAndGet(), "Две машины"));
-        accidentTypes.add(AccidentType.of(accidentTypeId.incrementAndGet(), "Машина и человек"));
-        accidentTypes.add(AccidentType.of(accidentTypeId.incrementAndGet(), "Машина и велосипед"));
+        accidentTypes.put(accidentTypeId.incrementAndGet(), AccidentType.of(accidentTypeId.get(), "Две машины"));
+        accidentTypes.put(accidentTypeId.incrementAndGet(), AccidentType.of(accidentTypeId.get(), "Машина и человек"));
+        accidentTypes.put(accidentTypeId.incrementAndGet(), AccidentType.of(accidentTypeId.get(), "Машина и велосипед"));
     }
 
     public Collection<AccidentType> findAll() {
-        return accidentTypes;
+        return accidentTypes.values();
     }
 }
