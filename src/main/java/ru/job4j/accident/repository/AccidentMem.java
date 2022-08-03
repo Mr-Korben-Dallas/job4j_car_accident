89845d52196cfb09e308c908a867029b2d0d11cb
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-08-03 07:50:57 +0400

    [#2092 #328002]:
    - refactoring;

--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -3,35 +3,34 @@ package ru.job4j.accident.repository;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.AccidentType;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+
+import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class AccidentMem {
     private final AtomicLong accidentId = new AtomicLong();
-    List<Accident> accidents = new ArrayList<>();
+    private final AtomicLong accidentMapId = new AtomicLong();
+    Map<Long, Accident> accidents = new HashMap<>();
 
     public AccidentMem() {
-        accidents.add(new Accident(accidentId.incrementAndGet(), "First", "Cur cotta messis?", "None of these ellipses will be lost in tragedies like collision courses in visions", AccidentType.of(1L, "Две машины")));
-        accidents.add(new Accident(accidentId.incrementAndGet(), "Second", "Rusticus domuss ducunt ad fuga.", "View oddly like a fantastic cosmonaut.", AccidentType.of(2L, "Машина и человек")));
-        accidents.add(new Accident(accidentId.incrementAndGet(), "Third", "Cursuss sunt indictios de emeritis solem.", "Walk without metamorphosis, and we won’t control a kahless.", AccidentType.of(2L, "Машина и человек")));
-        accidents.add(new Accident(accidentId.incrementAndGet(), "Fourth", "Ventuss sunt lunas de altus pars.", "Never imitate a transformator.", AccidentType.of(3L, "Машина и велосипед")));
-        accidents.add(new Accident(accidentId.incrementAndGet(), "Fifth", "Frondators ortum, tanquam varius calcaria.", "The space suit is virtually crazy.", AccidentType.of(3L, "Машина и велосипед")));
+        accidents.put(accidentMapId.incrementAndGet(), new Accident(accidentId.incrementAndGet(), "First", "Cur cotta messis?", "None of these ellipses will be lost in tragedies like collision courses in visions", AccidentType.of(1L, "Две машины")));
+        accidents.put(accidentMapId.incrementAndGet(), new Accident(accidentId.incrementAndGet(), "Second", "Rusticus domuss ducunt ad fuga.", "View oddly like a fantastic cosmonaut.", AccidentType.of(2L, "Машина и человек")));
+        accidents.put(accidentMapId.incrementAndGet(), new Accident(accidentId.incrementAndGet(), "Third", "Cursuss sunt indictios de emeritis solem.", "Walk without metamorphosis, and we won’t control a kahless.", AccidentType.of(2L, "Машина и человек")));
+        accidents.put(accidentMapId.incrementAndGet(), new Accident(accidentId.incrementAndGet(), "Fourth", "Ventuss sunt lunas de altus pars.", "Never imitate a transformator.", AccidentType.of(3L, "Машина и велосипед")));
+        accidents.put(accidentMapId.incrementAndGet(), new Accident(accidentId.incrementAndGet(), "Fifth", "Frondators ortum, tanquam varius calcaria.", "The space suit is virtually crazy.", AccidentType.of(3L, "Машина и велосипед")));
     }
 
     public Collection<Accident> findAll() {
-        return accidents;
+        return accidents.values();
     }
 
     public void createOrUpdate(Accident accident) {
         if (accident.getId() == 0) {
             accident.setId(accidentId.incrementAndGet());
-            accidents.add(accident);
+            accidents.put(accidentMapId.incrementAndGet(), accident);
         } else {
-            accidents.set(accident.getId().intValue() - 1, accident);
+            accidents.replace(accident.getId(), accident);
         }
     }
 
@@ -39,6 +38,6 @@ public class AccidentMem {
         if (accidents.size() < id) {
             return Optional.empty();
         }
-        return Optional.ofNullable(accidents.get(id.intValue() - 1));
+        return Optional.ofNullable(accidents.get(id));
     }
 }
